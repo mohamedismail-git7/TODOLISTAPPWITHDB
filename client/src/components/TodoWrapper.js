60f8f76418e9b9969f0bdaefc093c6fe65a37e1a
mohamedismail-git7 <mohamedismail9474@gmail.com>
2025-08-02 20:16:45 +0530

    updated the login page

--- client/src/components/TodoWrapper.js
+++ client/src/components/TodoWrapper.js
@@ -4,7 +4,7 @@ import { Todo } from './Todo';
 import { EditTodoForm } from './EditTodoForm';
 import axios from 'axios';
 
-const BASE_URL = 'https://0ygb0enhte.execute-api.ap-south-1.amazonaws.com/default';
+const BASE_URL = 'https://hydxtsla80.execute-api.ap-south-1.amazonaws.com/default';
 
 export const TodoWrapper = ({ userId }) => {
   const [todos, setTodos] = useState([]);
